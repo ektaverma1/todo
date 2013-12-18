16c9382116b64a1657c0b12450210a56f65f961b
ekta-verma <Ekta_Verma@external.mckinsey.com>
2013-12-18 12:26:27 -0800

    Todo application using haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'b021a11f16a062a7980f7cf82687fca03ad8daa256250773b2672f2f16983dd64bb65c4e8fa1c01ea639fa9dfc6858dd0793d77dfbb9469c2b5f536b8ea9d88f'
